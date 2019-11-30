57a4b20e8b800f5b1e80f38cfc92dcb057ec3208
uahmed23 <umer@uoguelph.ca>
2019-11-30 11:00:47 -0500

    Updates

---
+++ precache-manifest.9c558b9e75bbcb5da1bc0b9ebfeda347.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1936f443bfa66d298e69fbcf588fd772",
+    "url": "/project-cat/index.html"
+  },
+  {
+    "revision": "16cab9bc2e7293bc68c4",
+    "url": "/project-cat/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "14972c531055b3343589",
+    "url": "/project-cat/static/js/2.ec7f1a21.chunk.js"
+  },
+  {
+    "revision": "16cab9bc2e7293bc68c4",
+    "url": "/project-cat/static/js/main.4a676e08.chunk.js"
+  },
+  {
+    "revision": "810b8db145620f7dc7bd",
+    "url": "/project-cat/static/js/runtime-main.20a83de0.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/project-cat/static/media/logo.25bf045c.svg"
+  }
+]);
